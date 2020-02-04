f0ed56247a9a9201bde2b44b26efca8fcd26fbf6
jess.l <jess.l@kakaocorp.com>
2020-02-04 20:46:06 +0900

    initial commit

---
+++ src/main/java/com/example/rp/PracticeReactiveProgramming/controller/Temperature.java
@@ -0,0 +1,13 @@
+package com.example.rp.PracticeReactiveProgramming.controller;
+
+final class Temperature {
+    private final double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    public Temperature(double value) {
+        this.value = value;
+    }
+}
